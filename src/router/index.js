c27ac676c439698b407d22e38c02cc93c5ad44e7
Javier Sánchez <javiersanchez246@gmail.com>
2022-01-16 01:12:24 +0100

    Empezando registro

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
+import SignUp from '../views/SignUp.vue'
 
 const routes = [
   {
@@ -20,7 +21,12 @@ const routes = [
     path: '/login',
     name: 'Login',
     component: Login
-  }
+  },
+  {
+    path: "/registro",
+    name: "SignUp",
+    component: SignUp
+  },
 ]
 
 const router = createRouter({
