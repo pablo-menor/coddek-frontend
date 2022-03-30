eed2873fffea16dbe585083f4a646bb61cbe2c96
Pablo <pablomenor2001@gmail.com>
2022-03-30 17:36:39 +0200

    open and close menu done

---
+++ src/components/sidebar/CloseMenu.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container" @click="$emit('close-menu')">
+    <!-- <span class="material-icons-outlined icon"> navigate_next </span> -->
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="24px"
+      viewBox="0 0 24 24"
+      width="24px"
+      fill="#000000"
+    >
+      <path d="M0 0h24v24H0V0z" fill="none" />
+      <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12l4.58-4.59z" />
+    </svg>
+  </div>
+</template>
+<script>
+export default {
+  name: "CloseMenu",
+};
+</script>
+
+<style scoped>
+.container {
+  width: 40px;
+  height: 40px;
+  position: absolute;
+  top: 30%;
+  left: calc(1% - 20px);
+  background-color: #ffffff;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: right;
+}
+</style>
--- src/views/Dashboard/Dashboard.vue
+++ src/views/Dashboard/Dashboard.vue
@@ -3,6 +3,7 @@
     <!-- Menu mobile-->
     <sidebar-mobile class="menu-mobile"></sidebar-mobile>
     <side-arrow @open-menu="openMenu()" class="side-arrow"></side-arrow>
+    <close-menu @close-menu="closeMenu()" class="close-menu"></close-menu>
 
     <!-- buscador -->
     <!--Componente OffersPanel -->
@@ -11,25 +12,43 @@
 
 <script>
 import SidebarMobile from "../../components/sidebar/SidebarMobile.vue";
-import SideArrow from "../../components/sidebar/SideArrow.vue";
+import SideArrow from "../../components/sidebar/OpenMenu.vue";
+import CloseMenu from "../../components/sidebar/CloseMenu.vue";
 export default {
   name: "Dashboard",
   components: {
     SidebarMobile,
     SideArrow,
+    CloseMenu,
+  },
+  mounted() {
+    this.closeMenu();
   },
   methods: {
     openMenu() {
       let menuMobile = document.querySelector(".menu-mobile");
       menuMobile.style.marginLeft = "0";
       let sideArrow = document.querySelector(".side-arrow");
-        sideArrow.style.display = "none";
+      sideArrow.style.display = "none";
+      let closeMenu = document.querySelector(".close-menu");
+      closeMenu.style.display = "";
+    },
+    closeMenu() {
+      let menuMobile = document.querySelector(".menu-mobile");
+      menuMobile.style.marginLeft = "-98vw";
+      let sideArrow = document.querySelector(".side-arrow");
+      sideArrow.style.display = "";
+      let closeMenu = document.querySelector(".close-menu");
+      closeMenu.style.display = "none";
     },
   },
 };
 </script>
 
 <style scoped>
+.container{
+    overflow-x: hidden;
+}
 .menu-mobile {
   margin-left: -98vw;
   transition: 0.3s ease-in-out;
